0963b5dd322205caeaec47b0112edece2b9a2c36
Tarika514 <727723euit514@skcet.ac.in>
2023-11-17 10:07:02 +0530

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+//import Counter from "./Day3/Counter";   day3
+/*import Cart from "./Day 3/CoffeeShop/Cart";
+import Menu from "./Day 3/CoffeeShop/Menu";
+import Navbar from "./Day 3/CoffeeShop/Navbar";
+import categories from "./Day 3/CoffeeShop/Products";*/ //day3 cfeshop
+//import Buttons from "./Components/Buttons";  //day1
+import HelloWorld from "./Day2/HelloWorld";  //day2
+//import GokuTransformation from "./Day 3/GokuTransformation"; day 3 goku
+//import ToggleMessage from "./Day 3/ToggleMessage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      /*<div>
+         <Buttons>Buttons</Buttons>   
+        <Buttons color="green">Green</Buttons>
+        <Buttons color="red">Red</Buttons>
+        <Buttons color="blue">Blue</Buttons>
+        <Buttons color="yellow">Yellow</Buttons>
+        </div>*/  //day1
+        <div>   
+        <HelloWorld></HelloWorld>   
+        </div>  //day2 
+        /*<div>
+          <Cart></Cart>
+          <Menu></Menu>
+          <Navbar></Navbar>
+          <Products></Productss>
+    
+        </div>*/ //day3 cfe shop
+        /*<div>
+        <Counter></Counter>
+        </div>*/ //day3 counter
+        /*<div>
+          <GokuTransformation></GokuTransformation>
+        </div>*/ //goku day3
+        /*<div>
+          <ToggleMessage></ToggleMessage>
+        </div>*/ //day3 togglemsg
+        );
+      }
+      export default App;
